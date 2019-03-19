bcbcddfc8e59314b4225f766e1c801faaec21e86
Alison Mukoma <mukomalison@gmail.com>
2019-03-19 17:23:46 +0200

    add story detailed

--- django_project/content/urls.py
+++ django_project/content/urls.py
@@ -11,11 +11,22 @@ __copyright__ = ''
 from django.conf.urls import url
 from .views.home import HomeView
 from .views.home import AboutUsView
-from .views.success_story import SuccessStoryList
+from .views.success_story import (
+    SuccessStoryList,
+    SuccessStoryDetail)
 
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^about/$', AboutUsView.as_view(), name='about'),
-    url(r'^success/$', SuccessStoryList.as_view(), name='success')
+    url(r'^$',
+        HomeView.as_view(),
+        name='home'),
+    url(r'^about/$',
+        AboutUsView.as_view(),
+        name='about'),
+    url(r'^success/$',
+        SuccessStoryList.as_view(),
+        name='success'),
+    url(r'^story/(?P<story_pk>\d+)/detail/$',
+        SuccessStoryDetail.as_view(),
+        name='success_detail')
 ]
--- django_project/content/views/success_story.py
+++ django_project/content/views/success_story.py
@@ -7,9 +7,10 @@ __date__ = ('%s' % (datetime.now()))
 __license__ = ''
 __copyright__ = 'Broadreach Corperation'
 
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView, DetailView
 
 from ..models.success_story import SuccessStory
+from ..models.category import Category
 
 class SuccessStoryList(ListView):
     template_name = 'content/success_stories_list.html'
@@ -19,6 +20,26 @@ class SuccessStoryList(ListView):
         context = super(SuccessStoryList, self).get_context_data(**kwargs)
 
         success_qs = SuccessStory.objects.filter(published=True)
+
+        category_qs = Category.objects.all()[:5]
+        context['categories'] = category_qs
         context['success_stories'] = success_qs
 
         return context
+
+
+class SuccessStoryDetail(ListView):
+
+    template_name = 'content/success_story_detail.html'
+    model = SuccessStory
+
+    def get_context_data(self, **kwargs):
+        context = super(SuccessStoryDetail, self).get_context_data(**kwargs)
+
+        self.story_pk = self.kwargs.get('story_pk', None)
+        self.story_qs = SuccessStory.objects.get(pk=self.story_pk)
+        context['story'] = self.story_qs
+
+        return context
+
+
